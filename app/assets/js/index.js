ee1e333982b8cec6d50091131bcc747fa57b8dad
Gleriston <xpanium@gmail.com>
2023-07-30 11:36:42 -0300

    include functions range, moveMarque and execute video

--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -1,27 +1,74 @@
 const includes = (sections) => {
-  for(let i = 0; i < sections.length; i++ ){
+  for (let i = 0; i < sections.length; i++) {
     fetch(`${sections[i]}.html`)
       .then(response => response.text())
       .then(html => {
-          const footerDiv = document.getElementById(sections[i]);
-          footerDiv.innerHTML = html;
+        const footerDiv = document.getElementById(sections[i]);
+        footerDiv.innerHTML = html;
       });
   }
 }
 
 window.onload = function () {
   includes([
-  'about',
-  'calcBmi',
-  'cases',
-  'decisionMaking',
-  'dividerAnimation',
-  'footer',
-  'header',
-  'plans',
-  'schedules',
-  'socialNetwork',
-  'teachers',
-  'video'
-]);
+    'about',
+    'calcBmi',
+    'cases',
+    'decisionMaking',
+    'dividerAnimation',
+    'footer',
+    'header',
+    'plans',
+    'schedules',
+    'socialNetwork',
+    'teachers',
+    'video'
+  ]);
+  setTimeout(range, 100);
+  setTimeout(moveMarquee, 101);
+  setTimeout(executeVideo,102)
 };
+
+async function range() {
+  const range = document.getElementById("height");
+  const range2 = document.getElementById("weight");
+
+  const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+  };
+
+  const handleRangeInput = (e) => {
+    const value = +e.target.value;
+    const label = e.target.nextElementSibling;
+    const rangeWidth = getComputedStyle(e.target).getPropertyValue("width");
+    const labelWidth = getComputedStyle(label).getPropertyValue("width");
+
+    const numWidth = +rangeWidth.substring(0, rangeWidth.length - 2);
+    const numLabelWidth = +labelWidth.substring(0, labelWidth.length - 2);
+    const max = +e.target.max;
+    const min = +e.target.min;
+    const left =
+      value * (numWidth / max) -
+      numLabelWidth / 2 +
+      scale(value, min, max, 10, -10);
+    label.style.left = `${left}px`;
+    label.innerHTML = value;
+  };
+
+  range.addEventListener("input", handleRangeInput);
+  range2.addEventListener("input", handleRangeInput);
+}
+
+const marqueeText = document.getElementById("marqueeText");
+
+const scrollSpeed = 50;
+
+function moveMarquee() {
+  marqueeText.style.transform = `translateX(${scrollSpeed}px)`;
+
+  if (marqueeText.offsetLeft > marqueeText.offsetWidth) {
+    marqueeText.style.transform = "translateX(-100%)";
+  }
+
+  setTimeout(moveMarquee, 0);
+}
